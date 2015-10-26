99dbeec0b0ad2eb6bc1afaa445ff9bf7f7d0655a
karl <siliclone@gmail.com>
2015-10-26 17:23:13 -0400

    Refactored for foreground service.

--- app/src/main/java/edu/temple/androidservice/MainActivity.java
+++ app/src/main/java/edu/temple/androidservice/MainActivity.java
@@ -1,95 +1,33 @@
 package edu.temple.androidservice;
 
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.IBinder;
-import android.os.Message;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import org.json.JSONObject;
 
 public class MainActivity extends Activity {
-
-    QuoteService quoteService;
-    boolean connected;
-
-    ServiceConnection myConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            QuoteService.QuoteBinder binder = (QuoteService.QuoteBinder) service;
-            quoteService = binder.getService();
-            connected = true;
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            connected = false;
-        }
-    };
-
-    @Override
-    public void onStart(){
-        super.onStart();
-        Intent serviceIntent = new Intent(this, QuoteService.class);
-        bindService(serviceIntent, myConnection, Context.BIND_AUTO_CREATE);
-    }
-
-    @Override
-    public void onStop(){
-        super.onStop();
-        unbindService(myConnection);
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         findViewById(R.id.quoteButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (connected){
-                    quoteService.getQuote((
-                            ((EditText) findViewById(R.id.symbolEditText)).getText().toString()
-                    ), serviceHandler);
-                }
+                Intent serviceIntent = new Intent(MainActivity.this, QuoteService.class);
+                serviceIntent.putExtra("stock_symbol"
+                        , ((EditText) findViewById(R.id.symbolEditText)).getText().toString());
+                startService(serviceIntent);
+            }
+        });
+
+        findViewById(R.id.stopServiceButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(new Intent(MainActivity.this, QuoteService.class));
             }
         });
     }
-
-    Handler serviceHandler = new Handler(new Handler.Callback() {
-        @Override
-        public boolean handleMessage(Message msg) {
-            JSONObject responseObject = (JSONObject) msg.obj;
-            Stock currentStock = null;
-            try {
-                currentStock = new Stock(responseObject.getJSONObject("list")
-                        .getJSONArray("resources")
-                        .getJSONObject(0)
-                        .getJSONObject("resource")
-                        .getJSONObject("fields"));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            updateViews(currentStock);
-
-            return false;
-        }
-    });
-
-    private void updateViews(Stock currentStock) {
-        ((TextView)findViewById(R.id.companyName)).setText(currentStock.getName());
-        ((TextView)findViewById(R.id.stockPrice)).setText(String.valueOf("$" + currentStock.getPrice()));
-    }
-
 }
--- app/src/main/java/edu/temple/androidservice/QuoteService.java
+++ app/src/main/java/edu/temple/androidservice/QuoteService.java
@@ -1,11 +1,12 @@
 package edu.temple.androidservice;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
-import android.os.Binder;
-import android.os.Handler;
+import android.graphics.BitmapFactory;
 import android.os.IBinder;
-import android.os.Message;
+import android.util.Log;
 
 import org.json.JSONObject;
 
@@ -15,20 +16,24 @@ import java.net.URL;
 
 public class QuoteService extends Service {
 
-    IBinder quoteBinder = new QuoteBinder();
-
     @Override
     public IBinder onBind(Intent intent) {
-        return quoteBinder;
+        return null;
     }
 
-    public class QuoteBinder extends Binder {
-        QuoteService getService (){
-            return QuoteService.this;
-        }
+    @Override
+    public int onStartCommand (Intent intent, int flags, int startId){
+        getQuote(intent.getStringExtra("stock_symbol"));
+        return START_STICKY;
     }
 
-    public void getQuote(final String symbol, final Handler handler) {
+
+    @Override
+    public void onCreate(){
+        startForeground();
+    }
+
+    public void getQuote(final String symbol) {
         Thread t = new Thread() {
             @Override
             public void run() {
@@ -36,30 +41,49 @@ public class QuoteService extends Service {
                 URL stockQuoteUrl;
 
                 try {
+                    for (int i = 0; i < 10; i++) {
 
-                    stockQuoteUrl = new URL("http://finance.yahoo.com/webservice/v1/symbols/" + symbol + "/quote?format=json");
+                        stockQuoteUrl = new URL("http://finance.yahoo.com/webservice/v1/symbols/" + symbol + "/quote?format=json");
 
-                    BufferedReader reader = new BufferedReader(
-                            new InputStreamReader(
-                                    stockQuoteUrl.openStream()));
+                        BufferedReader reader = new BufferedReader(
+                                new InputStreamReader(
+                                        stockQuoteUrl.openStream()));
 
-                    String response = "", tmpResponse;
+                        String response = "", tmpResponse;
 
-                    tmpResponse = reader.readLine();
-                    while (tmpResponse != null) {
-                        response = response + tmpResponse;
                         tmpResponse = reader.readLine();
-                    }
+                        while (tmpResponse != null) {
+                            response = response + tmpResponse;
+                            tmpResponse = reader.readLine();
+                        }
 
-                    JSONObject stockObject = new JSONObject(response);
-                    Message msg = Message.obtain();
-                    msg.obj = stockObject;
-                    handler.sendMessage(msg);
+                        JSONObject stockObject = new JSONObject(response);
+                        Log.d("Saved stock data", stockObject.toString());
+                        Thread.sleep(2000);
+                    }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         };
         t.start();
+    }
+
+    private void startForeground(){
+        Notification.Builder n;
+
+        Intent i = new Intent(this, MainActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        i.setAction("SOME_ACTION");
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, i, 0);
+        n  = new Notification.Builder(this)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText("Your service is running")
+                .setSmallIcon(R.drawable.ic_small)
+                .setLargeIcon(BitmapFactory.decodeResource(this.getResources(), R.mipmap.ic_launcher))
+                .setContentIntent(pIntent)
+                .setAutoCancel(false);
+
+        startForeground(1234, n.build());
     }
 }
